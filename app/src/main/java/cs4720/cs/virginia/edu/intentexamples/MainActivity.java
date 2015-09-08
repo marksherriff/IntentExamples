91007d1f7331e3af44c02eafbf36caa9f50dc53c
Mark Sherriff <sherriff@virginia.edu>
2015-09-08 15:03:06 -0400

    Update for Map

--- app/src/main/java/cs4720/cs/virginia/edu/intentexamples/MainActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/intentexamples/MainActivity.java
@@ -1,12 +1,15 @@
 package cs4720.cs.virginia.edu.intentexamples;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -94,5 +97,20 @@ public class MainActivity extends AppCompatActivity {
         if (sendIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(sendIntent);
         }
+    }
+
+    public void openMap(View view) {
+        Uri geoLocation = null;
+        try {
+            geoLocation = Uri.parse("geo:38.0316114,-78.5107279?z=11");
+        } catch(Exception e) {
+            Log.e("Intent Example", "URI Exception");
+        }
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
     }
 }
